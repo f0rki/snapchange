bbd5fe52f7f4bc2a24e71f1e273d08009b1925d2
Cory Duplantis <corydu@amazon.com>
2023-07-14 10:51:10 -0500

    Add comments for the build.sh to explain ext2

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,13 @@ if [ ! -d ./fuzzer_template ]; then
   cp -r -L ../fuzzer_template .
 
   pushd fuzzer_template/qemu_snapshot/IMAGE
+
+  # For now, we're using e2cp to interact with the raw image to copy files
+  # into the image in preparation for the snapshot. e2cp needs an ext2 format
+  # instead of ext4.
   sed -i 's/ext4/ext2/' create-image.sh
+
+  # Create the image
   ./create-image.sh
   sudo rm -rf chroot
   popd
