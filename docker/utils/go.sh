cac11b0c602dba6b659565b9409ac4c3305edcb7
Michael Rodler <mrodler@amazon.de>
2023-07-24 19:51:51 +0000

    fixes for template copy-out in the snapchange container

--- docker/utils/go.sh
+++ docker/utils/go.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -z "$(ls "$SNAPSHOT_INPUT")" ]]; then
-  echo "[ERROR] No files provided for snapshot root filesystem in (copy to $SNAPSHOT_INPUT)" 
-fi
-
-if [[ "$1" = "template" ]]; then
+if [[ "$1" == "template" ]]; then
   echo "[+] extracting template files to $PWD"
   cp -r /snapchange/fuzzer_template/{.[!.]*,*} "$TEMPLATE_OUT"
   exit 0
+fi
+
+if [[ -n "$SNAPSHOT_INPUT" ]]; then
+  if [[ -z "$(ls "$SNAPSHOT_INPUT")" ]]; then
+    echo "[ERROR] No files provided for snapshot root filesystem in (copy to $SNAPSHOT_INPUT)" 
+  fi
 fi
 
 cd /snapchange/
