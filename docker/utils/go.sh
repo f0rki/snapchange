c091a233e4ae2b486f254f35253c3150ffad0e63
Michael Rodler <mrodler@amazon.de>
2023-07-26 16:10:16 +0000

    updated template output to not overwrite stuff

--- docker/utils/go.sh
+++ docker/utils/go.sh
@@ -5,8 +5,18 @@ source /snapchange/log.sh || { echo "Failed to source /snapchange/log.sh"; exit 
 set -e
 
 if [[ "$1" == "template" ]]; then
-  log_msg "extracting template files to $PWD"
-  cp -r /snapchange/fuzzer_template/{.[!.]*,*} "$TEMPLATE_OUT"
+  log_msg "extracting template files to $TEMPLATE_OUT"
+  for in_file in /snapchange/fuzzer_template/{.[!.]*,*}; do
+      out_file="$TEMPLATE_OUT/${in_file#*/snapchange/fuzzer_template/}"
+      if [[ -e "$out_file" ]]; then
+          log_warning "I will not overwrite file $out_file."
+      else
+          mkdir -p "$(dirname "$out_file")"
+          cp -r -n "$in_file" "$out_file"
+      fi
+  
+      chown -R "$SNAPSHOT_CHOWN_TO" "$out_file"
+  done
   exit 0
 fi
 
